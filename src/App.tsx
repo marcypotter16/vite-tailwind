5dd3f8a072cf9eb05ab563bfad9fcfdb4790ee9b
marcypotter16 <marcypotter16@gmail.com>
2023-07-02 22:26:54 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-gray-700 text-white rounded shadow'>
+        Hello
+      </div>
+
+    </>
+  )
+}
+
+// Write a react component for a home page
+
+
+export default App
